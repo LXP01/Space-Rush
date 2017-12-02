c0b1471278e0d8a687f5165ce94287b2343cf84b
GoldSchmiedchen <34176038+GoldSchmiedchen@users.noreply.github.com>
2017-12-02 17:23:54 +0100

    Movement Player(to be edited)

--- Space Rush/Assets/PlayerController.cs
+++ Space Rush/Assets/PlayerController.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Rigidbody rb;
+    public float force = 200f;
+    public Transform Player;
+
+    public float x=0f, y=0f;
+
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
+	void FixedUpdate ()
+    {
+        
+
+        y = Player.position.y; x = Player.position.x;
+
+        if (Input.GetKey("d"))
+        {rb.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);}
+        if (Input.GetKey("a"))
+        {rb.AddForce(-force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);}
+        if (Input.GetKey("w"))
+        {rb.AddForce(0, force * Time.deltaTime, 0, ForceMode.VelocityChange);}
+        if (Input.GetKey("s"))
+        { rb.AddForce(0, -force * Time.deltaTime, 0, ForceMode.VelocityChange); }
+
+       
+
+    }
+
+
 }
+
