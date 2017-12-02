1e4c55d150b8e9ac9ac58ed8835db42a77759623
GoldSchmiedchen <34176038+GoldSchmiedchen@users.noreply.github.com>
2017-12-02 22:01:13 +0100

    Spinner

--- Space Rush/Assets/Rotation.cs
+++ Space Rush/Assets/Rotation.cs
@@ -10,15 +10,17 @@ public class Rotation : MonoBehaviour {
     {
 
 
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //Subtracting player position with mous position
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //Subtracting player position with mouse position
+        difference.Normalize();
+        Vector3 rotation1;
+        
 
 
-        float rot = Mathf.Atan2(difference.y, difference.x); * Mathf.Rad2Deg; //find angle
-        transform.rotation = Quaternion.Euler(0f,0f,0+rot);
+        float rot = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; //find angle
+        rotation1.x = 0f; rotation1.y = 0f; rotation1.z = 0+rot;
+        transform.Rotate(rotation1);
 
-
-
-        Debug.Log(rot);
-        //Debug.Log(Input.mousePosition);
+        // Debug.Log(rot);
+        Debug.Log(Input.mousePosition);
     }
 }
