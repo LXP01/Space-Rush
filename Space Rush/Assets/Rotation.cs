db7e980fbdf898895e2043d79b6ddb0080690c97
GoldSchmiedchen <34176038+GoldSchmiedchen@users.noreply.github.com>
2017-12-02 21:22:40 +0100

    Update

---
+++ Space Rush/Assets/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //Subtracting player position with mous position
+
+
+        float rot = Mathf.Atan2(difference.y, difference.x); * Mathf.Rad2Deg; //find angle
+        transform.rotation = Quaternion.Euler(0f,0f,0+rot);
+
+
+
+        Debug.Log(rot);
+        //Debug.Log(Input.mousePosition);
+    }
+}
